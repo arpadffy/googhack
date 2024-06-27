8e0711d8aeea0f89b0d5ee9bf95941f702530582
Jacob Pesämaa <jacob.pesamaa@gmail.com>
2024-06-27 19:03:30 +0200

    chore: Refactor callGemini function in geminiService.js

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -33,20 +33,43 @@ const generativeModel = vertex_ai.preview.getGenerativeModel({
 });
 
 
-async function callGemini(promptContent, systemContent, previousChat) {
-  const req = {
+async function callGemini( prompt,
+  projectId = 'PROJECT_ID',
+  location = 'us-central1',
+  model = 'gemini-1.5-flash-001'
+) {
+  // Initialize Vertex with your Cloud project and location
+  const vertexAI = new VertexAI({project: projectId, location: location});
+
+  // Instantiate the model
+  const generativeModel = vertexAI.getGenerativeModel({
+    model: model,
+  });
+
+  const request = {
     contents: [
-      {role: 'user', parts: [{text: promptContent}]}
+      {role: 'user', parts: [{text: prompt}]},
+      {
+        role: 'model',
+        parts: [
+          {
+            functionCall: {
+              name: 'get_current_weather',
+              args: {location: 'Boston'},
+            },
+          },
+        ],
+      },
+      {role: 'user', parts: functionResponseParts},
     ],
+    tools: functionDeclarations,
   };
-
-  const streamingResp = await generativeModel.generateContentStream(req);
-
-  //for await (const item of streamingResp.stream) {
-  //  process.stdout.write('stream chunk: ' + JSON.stringify(item) + '\n');
-  //}
-
-  return JSON.stringify(await streamingResp.response);
+  const streamingResp = await generativeModel.generateContentStream(request);
+  var text="";
+  for await (const item of streamingResp.stream) {
+    text += JSON.stringify(item.candidates[0].content.parts[0].text);
+  }
+  return text;
 }
 
 module.exports = { callGemini };
